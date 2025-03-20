fb2f95cfeaf3a11a8eebd718d38fb08973b780bc
minho <pmmh9395@gmail.com>
2025-03-20 16:10:15 +0900

    Architecture of nestJs 끝

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// express 라우터 같은 존재 Url을 받아서 처리
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Get 메서드
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello')
+  sayHello(): string {
+    return this.appService.getHelloBro();
+    // return 'Hello guys!'; 위 처럼 서비스를 불러오는게 Nest.js의 컨벤션
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Nest!';
+  }
+  getHelloBro(): string {
+    return 'Hello Bros!!';
   }
 }
